421644452a5b910f6bbf39421fe47ebd36c7493a
sekvanto <zip.aioniplaitnereffid@gmail.com>
2020-07-22 14:23:38 +0300

    questions were moved to main()

--- include/questions.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUESTIONS_H
-#define QUESTIONS_H
-
-using namespace std;
-using namespace TgBot;
-
-void Questions(int, string, Message::Ptr);
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,10 @@
 #include <fstream>
 #include <tgbot/tgbot.h>
 #include "../include/getinfo.h"
-#include "../include/questions.h"
 
 using namespace std;
 using namespace TgBot;
 
-void ExtraInput();
 const int questionsNumber = 25;
 
 int main() {
@@ -18,7 +16,7 @@ int main() {
 	Bot bot(token);
 
 	// meme
-	const string photoFilePath = "sticker/meme.jpg";
+	const string photoFilePath = "../attachments/meme.jpg";
 	const string photoMimeType = "image/jpeg";
 	bot.getEvents().onCommand("meme", [&bot, &photoFilePath, &photoMimeType](Message::Ptr message) {
 		bot.getApi().sendPhoto(message->chat->id, InputFile::fromFile(photoFilePath, photoMimeType));
@@ -36,27 +34,6 @@ int main() {
 		bot.getApi().sendMessage(message->chat->id, "/go - начать прохождение активного опроса\n/get - получить описание какого-либо опроса\n\nАктивный опрос: 2. Длительность сна, приобретенные аномалии. Сновидения. Депривация сна\n\nПредыдущие опросы: 1. Зависимость ухудшения зрительной функции от наследственных и приобретенных факторов");
 	});
 
-
-	// go
-	int iterator = 0;
-	bot.getEvents().onCommand("go", [&bot, &iterator](Message::Ptr message) {
-			string filename = "..qbot_extra/data/" + to_string(message->from->id);
-			ifstream check_file (filename);
-			if (check_file.is_open()){
-				char temp_c = check_file.get();
-				while(check_file.good()){
-					++iterator;
-					temp_c = check_file.get();
-				}
-				--iterator;
-				check_file.close();
-			}
-			if(message->chat->type == Chat::Type::Private)
-				for(int i = iterator; i < questionsNumber; i++)
-					Questions(i, filename, message);
-			else bot.getApi().sendMessage(message->chat->id, "Напиши в личку для прохождения");
-	});
-	
 
 	// get
 	bot.getEvents().onCommand("get", [&bot](Message::Ptr message) {
@@ -84,6 +61,63 @@ int main() {
 	});
 
 
+	// go
+	int iterator = 0;
+	bot.getEvents().onCommand("go", [&bot, &iterator](Message::Ptr message) {
+			if(message->chat->type != Chat::Type::Private)
+					bot.getApi().sendMessage(message->chat->id, "Напиши в личку для прохождения");
+
+			// начинаем опрос
+
+			else {
+				string filename = "../../qbot_extra/data/" + to_string(message->from->id);
+				ifstream check_file (filename);
+				if (check_file.is_open()){
+					char temp_c = check_file.get();
+					while(check_file.good()){
+						++iterator;
+						temp_c = check_file.get();
+					}
+					check_file.close();
+				}
+			
+				string Strings[questionsNumber+1];
+				int index = 0;
+				ifstream getq ("../attachments/questions");
+				if (getq.is_open()) while(getline(getq, Strings[index])) ++index;
+				getq.close();
+				ofstream write;
+				write.open(filename.c_str(), ios::out | ios::app);
+				
+				// 0) вопрос 1.1
+
+				InlineKeyboardMarkup::Ptr k11(new InlineKeyboardMarkup);
+				vector<InlineKeyboardButton::Ptr> r111;
+				InlineKeyboardButton::Ptr b111(new InlineKeyboardButton);
+				InlineKeyboardButton::Ptr b112(new InlineKeyboardButton);
+				b111->text = "Нет";
+				b111->callbackData = "No";
+				b112->text = "Да";
+				b112->callbackData = "Yes";
+				r111.push_back(b111);
+				r111.push_back(b112);
+				k11->inlineKeyboard.push_back(r111);
+			
+				if (iterator == 0) {
+					bot.getApi().sendMessage(message->chat->id, Strings[0], false, 0, k11);
+					bot.getEvents().onCallbackQuery([&bot, &k11, &write](CallbackQuery::Ptr query) {
+							bot.getApi().answerCallbackQuery(query->id);
+							if (StringTools::startsWith(query->data, "No")) {
+								//write << "Нет. ";
+							}
+					});
+				}
+
+				write.close();
+			}
+	});
+	
+
 	signal(SIGINT, [](int s) {
         printf("SIGINT got\n");
         exit(0);
@@ -103,13 +137,4 @@ int main() {
     }
 
     return 0;
-}
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-void ExtraInput() {
 }
